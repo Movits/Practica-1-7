55e26e060ab3fad6d4c34112dae663b4a9d30177
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-22 21:17:11 -0300

    EjB

--- EjA.js
+++ EjA.js
@@ -1,9 +1,9 @@
 // Maria tiene 5 manzanas, Pedro tiene 8 y Susana tiene 9. Si juntan todas las manzanas en una cesta, cuantas manzanas tienen? //
 
-Maria = 5;
-Pedro = 8;
-Susana = 9;
+var Maria = 5;
+var Pedro = 8;
+var Susana = 9;
 
-TotalManzanas = Maria + Pedro + Susana + " manzanas"
+var TotalManzanas = Maria + Pedro + Susana + " manzanas"
 
 console.log(TotalManzanas)
---
+++ EjB.js
@@ -0,0 +1,9 @@
+// Juan tiene 28 magdalenas. Tiene 12 amigas y le da una magdalena a cada una, y otra para el. Cuantas manzanas quedan? //
+
+var Juan = 28;
+var Amigas = 12;
+
+var RestoMagdalenas = Juan % Amigas
+var Manzanas = null;
+
+console.log(Manzanas)
