8d9d809bcbc468c79c73d51393943f32e9935092
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-22 21:51:24 -0300

    EjE

---
+++ EjE.js
@@ -0,0 +1,8 @@
+// Juan tiene que conducir exactamente 2,6 kilometros desde su pueblo hasta Toledo para comprar un regalo para su tio en la tienda de Marcelina.
+// Despues, desde la puerta de la tienda hasta la casa de su tio recorrera justo 691 metros. Podriamos expresar la distancia total recorrida en milimetros?
+
+var PuebloToledo = 2.6 * 1000;
+var TiendaTio = 691;
+var DistTotal = (PuebloToledo + TiendaTio) * 1000;
+
+console.log("La distancia total en milimetros es: " + DistTotal)
