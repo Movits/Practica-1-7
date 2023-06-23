1f5e48d330fba86a97ff49164be1f716978aec8c
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-22 21:24:33 -0300

    EjC

---
+++ EjC.js
@@ -0,0 +1,7 @@
+// Paco se compro una hamburguesa que costaba $35 y un refresco de $18. Cuanto dinero pago en total? //
+
+var Hamburguesa = 35;
+var Refresco = 18;
+var Total = Hamburguesa + Refresco;
+
+console.log("El total que Paco gasto fue: $" + Total)
